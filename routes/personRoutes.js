d2227f41cd8b958772f29c828b149f11ae32b33d
Md Aftab <maftabuddin76@gmail.com>
2026-06-20 22:51:05 +0530

    lastest shift mongoDB to Online server

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Person = require('./../models/Person');
+const Person = require('../models/Person');
 
 router.post('/', async(req, res) => {
     try{
